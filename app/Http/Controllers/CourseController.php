cada944556607655b754ca305184016d3627712b
motuget <7601841@qq.com>
2018-10-08 03:30:25 +0800

    添加 course 控制器

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CourseController extends Controller
+{
+    public function index()
+    {
+        return '123';
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+}
